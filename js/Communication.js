b62afa243474da7baecaf0dca5bafc1ce0338b3f
anxin <rengongzhineng369@gmail.com>
2024-04-25 22:15:16 +0800

    js有误，更新一下

--- js/Communication.js
+++ js/Communication.js
@@ -1,5 +1,6 @@
 // Communication.js
 twikoo.init({
     envId: 'https://www.xn--7gq455a.site/',  // 替换为你的云开发环境ID
-    region: false  // 替换为你的云开发环境区域，例如：ap-guangzhou
+    el: '#twikoo', // 容器元素
+    // region: "ap-guangzhou"  // 替换为你的云开发环境区域，例如：ap-guangzhou
 });
