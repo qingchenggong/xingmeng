732a97848d4fafd5aee0b21d8488e6dd53901813
anxin <rengongzhineng369@gmail.com>
2024-05-09 18:28:02 +0800

    启用最新项目
    依然是index.html为入口

---
+++ js/Communication.js
@@ -0,0 +1,4 @@
+twikoo.init({
+    envId: 'https://www.xn--7gq455a.site/',
+    el: '#twikoo',
+});
---
+++ season/js/video.js
@@ -0,0 +1,7 @@
+var v=document.getElementById('video');
+var h=document.getElementById('hotpot');
+v.onmouseover=function (){
+    v.style.width='400px';
+    v.style.height='400px';
+    v.style.marginTop='5%';
+}
